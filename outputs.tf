9cf9402d512865bfa8139c52a71cd3fc7559025b
rossijonas <rossijonas@pm.me>
2022-08-29 13:13:01 -0300

    output bucket arn and domain_name

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "s3_bucket_id" {
   description = "The name of the bucket."
   value       = aws_s3_bucket.alldrops_info.bucket
 }
+
+output "s3_bucket_website_arn" {
+  description = "The ARN of the bucket"
+  value       = aws_s3_bucket.alldrops_info.arn
+}
+
+output "s3_bucket_website_domain_name" {
+  description = "The bucket domain name"
+  value       = aws_s3_bucket.alldrops_info.bucket_domain_name
+}
