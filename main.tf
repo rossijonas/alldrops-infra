c7b8685599b68c659966befabce7597b02eda88b
rossijonas <rossijonas@pm.me>
2022-08-30 11:11:14 -0300

    cloudfront access the bucket

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "random_pet" "this" {
 }
 
 # -----------------------------------------------------------------------------
-# aws resources
+# aws bucket
 # -----------------------------------------------------------------------------
 
 resource "aws_s3_bucket" "alldrops_info" {
@@ -35,4 +35,47 @@ resource "aws_s3_bucket_website_configuration" "alldrops_info_config" {
 resource "aws_s3_bucket_policy" "alldrops_info_policy" {
   bucket = aws_s3_bucket.alldrops_info.bucket
   policy = templatefile("s3-policy.json", { bucket = local.bucket_name })
+}
+
+# -----------------------------------------------------------------------------
+# aws cloudfront
+# -----------------------------------------------------------------------------
+
+resource "aws_cloudfront_distribution" "alldrops_info" {
+  enabled         = true
+  is_ipv6_enabled = true
+
+  origin {
+    domain_name = aws_s3_bucket.alldrops_info.bucket_domain_name
+    origin_id   = aws_s3_bucket.alldrops_info.bucket
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  default_cache_behavior {
+    target_origin_id = aws_s3_bucket.alldrops_info.bucket
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 7200
+    max_ttl                = 86400
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
