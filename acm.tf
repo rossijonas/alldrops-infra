bc666e1b0a7519ce6d705a1fb8f0921885ee0a83
rossijonas <rossijonas@pm.me>
2022-09-02 14:32:11 -0300

    fix typo in subject alternative names

--- acm.tf
+++ acm.tf
@@ -1,6 +1,6 @@
 resource "aws_acm_certificate" "ssl_certificate" {
   domain_name               = var.domain_name
-  subject_alternative_names = ["*.{var.domain_name}"]
+  subject_alternative_names = ["*.${var.domain_name}"]
   validation_method         = "DNS"
 
   lifecycle {
