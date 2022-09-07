f5a0bbcfd2dd7c594fd8bab695c0832dfd70b718
rossijonas <rossijonas@pm.me>
2022-09-07 10:44:47 -0300

    remove CAA record

--- route53.tf
+++ route53.tf
@@ -2,13 +2,13 @@ resource "aws_route53_zone" "all_drops_zone" {
   name = var.domain_name
 }
 
-resource "aws_route53_record" "alldrops_record" {
-  zone_id = aws_route53_zone.all_drops_zone.zone_id
-  name    = tolist(aws_acm_certificate.ssl_certificate.domain_validation_options)[0].resource_record_name
-  type    = tolist(aws_acm_certificate.ssl_certificate.domain_validation_options)[0].resource_record_type
-  records = [tolist(aws_acm_certificate.ssl_certificate.domain_validation_options)[0].resource_record_value]
-  ttl     = var.route53_ttl
-}
+# resource "aws_route53_record" "alldrops_record" {
+#   zone_id = aws_route53_zone.all_drops_zone.zone_id
+#   name    = tolist(aws_acm_certificate.ssl_certificate.domain_validation_options)[0].resource_record_name
+#   type    = tolist(aws_acm_certificate.ssl_certificate.domain_validation_options)[0].resource_record_type
+#   records = [tolist(aws_acm_certificate.ssl_certificate.domain_validation_options)[0].resource_record_value]
+#   ttl     = var.route53_ttl
+# }
 
 resource "aws_route53_record" "alldrops_a_record" {
   zone_id = aws_route53_zone.all_drops_zone.zone_id
