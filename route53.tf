1927806af28ac63c88abbb71813624b9748e3690
rossijonas <rossijonas@pm.me>
2022-09-07 10:41:56 -0300

    create A record pointing to distribution

--- route53.tf
+++ route53.tf
@@ -10,6 +10,18 @@ resource "aws_route53_record" "alldrops_record" {
   ttl     = var.route53_ttl
 }
 
+resource "aws_route53_record" "alldrops_a_record" {
+  zone_id = aws_route53_zone.all_drops_zone.zone_id
+  name    = tolist(aws_acm_certificate.ssl_certificate.domain_validation_options)[0].resource_record_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.alldrops_info.domain_name
+    zone_id                = aws_cloudfront_distribution.alldrops_info.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
+
 resource "aws_route53_record" "alldrops_caa_record" {
   zone_id = aws_route53_zone.all_drops_zone.zone_id
   name    = var.domain_name
